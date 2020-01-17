659e742a80c2cde5ada37aa36498d1ea8b755dbd
Fredrik Malmo <fredrik@kodeworks.no>
2020-01-17 15:27:29 +0100

    Clear main func
    GH Actions go!

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,4 @@ mod kasiski_test;
 mod matrix;
 mod miller_rabin;
 
-fn main() {
-    println!("========== OUTPUT ==========");
-    println!(
-        "{:?}",
-        matrix::dot(
-            vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]],
-            vec![vec![10, 11, 12], vec![13, 14, 15], vec![16, 17, 18]]
-        )
-    )
-}
+fn main() {}
